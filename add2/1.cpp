6c0a0c8590369c72d1c317a404fdf9b26248e7e8
lysa0 <alysovlt@gmail.com>
2017-03-03 20:13:42 +0300

    .

--- add2/1.cpp
+++ add2/1.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 #define eps 1e-7
+#define delta 1e-9
 using namespace std;
 void solve();
 bool degThr(const int & a);
@@ -7,11 +8,11 @@ int main(){
   solve();
 }
 void solve(){
-  int a[5]{15,27,243,100, 59049};
+  int a[5]{15,27,243,100, 387420488};
   for (int *it = a; it<a+5; it++){
       cout << *it << " its " << degThr(*it) << " degree three\n"; 
   }
 }
 bool degThr(const int &a){
-  return (fabs(a-pow(3,(int)(log(a)/log(3)+eps))<eps));
+  return (fabs(a-(pow(3,(int)(log(a)/log(3)+eps))))<delta);
 }
